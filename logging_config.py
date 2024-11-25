892f1a226b26454954ec694e640901f63b1e5dcf
shaothinkbookubuntu <157350686@qq.com>
2024-11-25 09:51:40 +0800

    uu

--- logging_config.py
+++ logging_config.py
@@ -1,12 +1,18 @@
-
+import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
 def setup_logging():
+    log_directory = "/home/shao/dtu_hub/log"
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
     logger = logging.getLogger("dtu_hub")
     logger.setLevel(logging.INFO)
     handler = TimedRotatingFileHandler(
-        "./log/dtu_hub.log", when="midnight", interval=1, backupCount=30)
+        os.path.join(log_directory, "dtu_hub.log"),
+        when="midnight",
+        interval=1,
+        backupCount=30)
     handler.suffix = "%Y%m%d"
     handler.maxBytes = 10 * 1024 * 1024  # 10MB
     formatter = logging.Formatter(
